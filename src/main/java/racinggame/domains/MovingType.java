0ea3efdc57289fd8230489bdefbeb6477e15e9ee
cobiyu <cobiyu90@gmail.com>
2021-10-11 21:58:33 +0900

    feat(MovingType) : 자동차 이동 타입 로직 추가

--- src/main/java/racinggame/domains/MovingType.java
+++ src/main/java/racinggame/domains/MovingType.java
@@ -4,10 +4,42 @@ package racinggame.domains;
  * @author cobiyu
  */
 public enum MovingType {
-	GO,
-	STOP;
+	GO(1),
+	STOP(0);
 
+	/**
+	 * 레이싱에 사용되는 기준값
+	 */
+	public static final int MOVING_TYPE_VALUE = 4;
+	/**
+	 * Type별 가중치
+	 */
+	private final int weighting;
+
+	/**
+	 * constructor
+	 */
+	MovingType(int weighting) {
+		this.weighting = weighting;
+	}
+
+	/**
+	 * weighting getter
+	 */
+	public int getWeighting() {
+		return weighting;
+	}
+
+	/**
+	 * 숫자에 맞는 MovingType 생성
+	 *
+	 * @param racingNumber MovingType 생성에 판단할 숫자
+	 * @return GO or STOP
+	 */
 	public static MovingType of(int racingNumber) {
-		return GO;
+		if (racingNumber >= MOVING_TYPE_VALUE) {
+			return GO;
+		}
+		return STOP;
 	}
 }
