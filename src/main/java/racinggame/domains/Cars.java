41e0d8f6277cd756b18281ea62c2fd6de5243bbb
cobiyu <cobiyu90@gmail.com>
2021-10-12 19:55:43 +0900

    feat(Car) : 자동차 N대 이동, 우승자 추출 로직 작성

--- src/main/java/racinggame/domains/Cars.java
+++ src/main/java/racinggame/domains/Cars.java
@@ -1,5 +1,7 @@
 package racinggame.domains;
 
+import nextstep.utils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,20 +9,39 @@ import java.util.List;
  * @author cobiyu
  */
 public class Cars {
+	private static final int MIN_RACING_NUMBER = 0;
+	private static final int MAX_RACING_NUMBER = 9;
 	private final List<Car> carList;
 
+	/**
+	 * constructor
+	 */
 	public Cars(List<Car> carList) {
 		this.carList = carList;
 	}
 
+	/**
+	 * 차량별로 추출한 0~9 random 숫자에 맞게 차량 이동  
+	 */
 	public void movingByRandomNumber() {
-		
+		for (Car car : carList) {
+			int randomNumber = Randoms.pickNumberInRange(MIN_RACING_NUMBER, MAX_RACING_NUMBER);
+			MovingType movingType = MovingType.of(randomNumber);
+			car.moving(movingType);
+		}
 	}
 
+	/**
+	 * 우승 자동차 추출
+	 */
 	public List<Car> getWinnerCars() {
-		List<Car> carList = new ArrayList<>();
+		List<Car> winnerCarList = new ArrayList<>();
 
-		return carList;
+		for (Car car : carList) {
+			addWinnerCarsIfMaxDistance(winnerCarList, car);
+		}
+
+		return winnerCarList;
 	}
 
 	/**
@@ -28,5 +49,32 @@ public class Cars {
 	 */
 	public List<Car> getCarList() {
 		return carList;
+	}
+
+	/**
+	 * 자동차 중 최대 이동 자동차의 거리 추출
+	 */
+	private int getMaxDistance() {
+		int maxDistance = 0;
+
+		for (Car car : carList) {
+			maxDistance = Math.max(maxDistance, car.getMovingDistance());
+		}
+
+		return maxDistance;
+	}
+
+	/**
+	 * 우승 자동차 목록 추가
+	 *
+	 * @param winnerCarList 우승 자동차 목록
+	 * @param car 우승인지 판별한 자동차
+	 */
+	private void addWinnerCarsIfMaxDistance(List<Car> winnerCarList, Car car) {
+		int maxDistance = getMaxDistance();
+
+		if (car.getMovingDistance() == maxDistance) {
+			winnerCarList.add(car);
+		}
 	}
 }
