0381149097bec83f4fb5d05368ba2ba4865a533e
cobiyu <cobiyu90@gmail.com>
2021-10-12 23:29:21 +0900

    feat(ExceptionHandler) : 예외 처리기 추가

---
+++ src/main/java/racinggame/exceptions/ExceptionHandler.java
@@ -0,0 +1,13 @@
+package racinggame.exceptions;
+
+/**
+ * @author cobiyu
+ */
+public class ExceptionHandler {
+	/**
+	 * exception handling
+	 */
+	public static void handling(Exception ex) {
+		System.out.println("[ERROR] " + ex.getMessage());
+	}
+}
